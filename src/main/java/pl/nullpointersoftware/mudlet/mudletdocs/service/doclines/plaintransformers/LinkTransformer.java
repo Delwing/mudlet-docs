eaf372d4c3048f623994ce0e88b2af2e72c9f08a
Piotr Wilczynski <piotr.wilczynski@bisnode.com>
2021-02-04 12:34:49 +0100

    correct link replacer

--- src/main/java/pl/nullpointersoftware/mudlet/mudletdocs/service/doclines/plaintransformers/LinkTransformer.java
+++ src/main/java/pl/nullpointersoftware/mudlet/mudletdocs/service/doclines/plaintransformers/LinkTransformer.java
@@ -8,7 +8,7 @@ public class LinkTransformer implements PlainTransformer {
     @Override
     public String transform(String line) {
         line = line.replaceAll("\\[(\\S+) ((?:\\w+ ?)+)]","[$2]($1)");
-        line = line.replaceAll("\\[\\[(.*)\\|(.*)]]", "[$2](https://wiki.mudlet.org/w/$1)");
+        line = line.replaceAll("\\[\\[([^|]*)\\|([^]]*)]]", "[$2](https://wiki.mudlet.org/w/$1)");
         return line;
     }
 }
