6c14e681f764e0787832101b493c5cdf30afbc02
Piotr Wilczynski <delwing@gmail.com>
2021-08-07 16:57:15 +0200

    links with spaces fixed

--- src/main/java/pl/nullpointersoftware/mudlet/mudletdocs/service/doclines/plaintransformers/LinkTransformer.java
+++ src/main/java/pl/nullpointersoftware/mudlet/mudletdocs/service/doclines/plaintransformers/LinkTransformer.java
@@ -8,11 +8,11 @@ public class LinkTransformer implements PlainTransformer {
 
     @Override
     public String transform(String line) {
-        line = line.replaceAll("\\[(\\S+) ((?:\\w+ ?)+)]","[$2]($1)");
-        line = line.replaceAll("\\[\\[(Manual:.*?)\\|([^]]*)]]", "[$2](https://wiki.mudlet.org/w/$1)");
-        line = line.replaceAll("\\[\\[(Manual:.*?)]]", "[$1](https://wiki.mudlet.org/w/$1)");
-        line = line.replaceAll("\\[\\[([^|]*)\\|([^]]*)]]", "[$2](https://wiki.mudlet.org/w/Manual:Lua_Functions$1)");
-        line = line.replaceAll("\\[\\[([^|]*)]]", "[$1](https://wiki.mudlet.org/w/Manual:Lua_Functions$1)");
+        line = line.replaceAll("\\[(\\S+) ((?:\\w+ ?)+)]","[$2](<$1>)");
+        line = line.replaceAll("\\[\\[(Manual:.*?)\\|([^]]*)]]", "[$2](<https://wiki.mudlet.org/w/$1>)");
+        line = line.replaceAll("\\[\\[(Manual:.*?)]]", "[$1](<https://wiki.mudlet.org/w/$1>)");
+        line = line.replaceAll("\\[\\[([^|]*)\\|([^]]*)]]", "[$2](<https://wiki.mudlet.org/w/Manual:Lua_Functions$1>)");
+        line = line.replaceAll("\\[\\[([^|]*)]]", "[$1](<https://wiki.mudlet.org/w/Manual:Lua_Functions$1>)");
         return line;
     }
 
